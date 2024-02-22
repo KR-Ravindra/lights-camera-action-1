2863301cf75db9b4b836d1b2015dcd833878b1e5
Raja Ravindra Kathi <rajaravindra919@gmail.com>
2024-02-22 15:01:29 -0800

    Create surf.cpp

---
+++ surf.cpp
@@ -0,0 +1,152 @@
+#include "surf.h"
+#include "extra.h"
+using namespace std;
+
+namespace
+{
+    
+    // We're only implenting swept surfaces where the profile curve is
+    // flat on the xy-plane.  This is a check function.
+    static bool checkFlat(const Curve &profile)
+    {
+        for (unsigned i=0; i<profile.size(); i++)
+            if (profile[i].V[2] != 0.0 ||
+                profile[i].T[2] != 0.0 ||
+                profile[i].N[2] != 0.0)
+                return false;
+    
+        return true;
+    }
+}
+Surface makeSurfRev(const Curve &profile, unsigned steps)
+{
+    Surface surface;
+    float stepSize = 2.0f * M_PI / steps;
+
+    for (unsigned i = 0; i < steps; ++i)
+    {
+        float theta = i * stepSize;
+        Matrix4f rotation = Matrix4f::rotateY(theta);
+
+        for (const CurvePoint &point : profile)
+        {
+            Vector3f position = rotation.transformPoint(point.V);
+            Vector3f normal = rotation.transformVector(point.N);
+            surface.VV.push_back(position);
+            surface.VN.push_back(normal);
+        }
+    }
+
+    return surface;
+}
+
+Surface makeGenCyl(const Curve &profile, const Curve &sweep)
+{
+    Surface surface;
+
+    for (const CurvePoint &sweepPoint : sweep)
+    {
+        Matrix4f translation = Matrix4f::translation(sweepPoint.V);
+
+        for (const CurvePoint &profilePoint : profile)
+        {
+            Vector3f position = translation.transformPoint(profilePoint.V);
+            Vector3f normal = translation.transformVector(profilePoint.N);
+            surface.VV.push_back(position);
+            surface.VN.push_back(normal);
+        }
+    }
+
+    return surface;
+}
+
+void drawSurface(const Surface &surface, bool shaded)
+{
+    // Save current state of OpenGL
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+
+    if (shaded)
+    {
+        // This will use the current material color and light
+        // positions.  Just set these in drawScene();
+        glEnable(GL_LIGHTING);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+        // This tells openGL to *not* draw backwards-facing triangles.
+        // This is more efficient, and in addition it will help you
+        // make sure that your triangles are drawn in the right order.
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+    }
+    else
+    {        
+        glDisable(GL_LIGHTING);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        
+        glColor4f(0.4f,0.4f,0.4f,1.f);
+        glLineWidth(1);
+    }
+
+    glBegin(GL_TRIANGLES);
+    for (unsigned i=0; i<surface.VF.size(); i++)
+    {
+        glNormal(surface.VN[surface.VF[i][0]]);
+        glVertex(surface.VV[surface.VF[i][0]]);
+        glNormal(surface.VN[surface.VF[i][1]]);
+        glVertex(surface.VV[surface.VF[i][1]]);
+        glNormal(surface.VN[surface.VF[i][2]]);
+        glVertex(surface.VV[surface.VF[i][2]]);
+    }
+    glEnd();
+
+    glPopAttrib();
+}
+
+void drawNormals(const Surface &surface, float len)
+{
+    // Save current state of OpenGL
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+
+    glDisable(GL_LIGHTING);
+    glColor4f(0,1,1,1);
+    glLineWidth(1);
+
+    glBegin(GL_LINES);
+    for (unsigned i=0; i<surface.VV.size(); i++)
+    {
+        glVertex(surface.VV[i]);
+        glVertex(surface.VV[i] + surface.VN[i] * len);
+    }
+    glEnd();
+
+    glPopAttrib();
+}
+
+void outputObjFile(ostream &out, const Surface &surface)
+{
+    
+    for (unsigned i=0; i<surface.VV.size(); i++)
+        out << "v  "
+            << surface.VV[i][0] << " "
+            << surface.VV[i][1] << " "
+            << surface.VV[i][2] << endl;
+
+    for (unsigned i=0; i<surface.VN.size(); i++)
+        out << "vn "
+            << surface.VN[i][0] << " "
+            << surface.VN[i][1] << " "
+            << surface.VN[i][2] << endl;
+
+    out << "vt  0 0 0" << endl;
+    
+    for (unsigned i=0; i<surface.VF.size(); i++)
+    {
+        out << "f  ";
+        for (unsigned j=0; j<3; j++)
+        {
+            unsigned a = surface.VF[i][j]+1;
+            out << a << "/" << "1" << "/" << a << " ";
+        }
+        out << endl;
+    }
+}
